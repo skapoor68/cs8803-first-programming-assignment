34917e8240a127b30917b0e66902f4a15d0d4db1
jasminelli <jasco88017@gmail.com>
2024-08-27 19:56:00 -0400

    add icons

--- components/WeatherDisplay.tsx
+++ components/WeatherDisplay.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface WeatherData {
   name: string;
@@ -20,13 +21,30 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ weatherData }) => {
     return null;
   }
 
+  const weatherDescription = weatherData.weather[0].main;
+
   return (
     <View style={styles.container}>
       <Text style={styles.cityName}>{weatherData.name}</Text>
       <Text style={styles.temperature}>{weatherData.main.temp.toFixed(1)}°F</Text>
-      <Text style={styles.weatherCondition}>{weatherData.weather[0].main}</Text>
+      <Icon name={parseWeatherDescription(weatherDescription)} size={70}></Icon>
+      <Text style={styles.weatherCondition}>{weatherDescription}</Text>
     </View>
   );
+};
+
+const parseWeatherDescription = (description: string) => {
+	const standardizedDescription = description.trim().toLowerCase();
+	
+	if (standardizedDescription.includes("cloud")) {
+		return 'weather-cloudy';
+	} else if (standardizedDescription.includes("sun")) {
+		return 'weather-sunny';
+	} else if (standardizedDescription.includes("rain")) {
+		return 'weather-rainy';
+	} else {
+		return 'weather-cloudy';
+	}
 };
 
 const styles = StyleSheet.create({
